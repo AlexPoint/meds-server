c1d2dc3c22e2900d1f1e66b94a4f84b42d917bc8
Alex <pointalexandre@gmail.com>
2016-07-12 22:44:36 +0200

    Basic express setup for API

--- index.js
+++ index.js
@@ -1,50 +1,36 @@
-// Import the required libraries
-var graphql = require('graphql');
-var graphqlHTTP = require('express-graphql');
-var express = require('express');
+// server.js
 
-// Import the data you created above
-var data = require('./data.json');
+// BASE SETUP
+// =============================================================================
 
-// Define the User type with two string fields: `id` and `name`.
-// The type of User is GraphQLObjectType, which has child fields
-// with their own types (in this case, GraphQLString).
-var userType = new graphql.GraphQLObjectType({
-  name: 'User',
-  fields: {
-    id: { type: graphql.GraphQLString },
-    name: { type: graphql.GraphQLString },
-  }
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 9090;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:9090/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-// Define the schema with one top-level field, `user`, that
-// takes an `id` argument and returns the User with that ID.
-// Note that the `query` is a GraphQLObjectType, just like User.
-// The `user` field, however, is a userType, which we defined above.
-var schema = new graphql.GraphQLSchema({
-  query: new graphql.GraphQLObjectType({
-    name: 'Query',
-    fields: {
-      user: {
-        type: userType,
-        // `args` describes the arguments that the `user` query accepts
-        args: {
-          id: { type: graphql.GraphQLString }
-        },
-        // The resolve function describes how to "resolve" or fulfill
-        // the incoming query.
-        // In this case we use the `id` argument from above as a key
-        // to get the User from `data`
-        resolve: function (_, args) {
-          return data[args.id];
-        }
-      }
-    }
-  })
-});
+// more routes for our API will happen here
 
-express()
-  .use('/graphql', graphqlHTTP({ schema: schema, pretty: true }))
-  .listen(3000);
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
 
-console.log('GraphQL server running on http://localhost:3000/graphql');
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
