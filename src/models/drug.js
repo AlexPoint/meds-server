cddafa67396d249a803a2729d65116f9ec3e181b
Alexandre Point <apoint@fleex.tv>
2016-07-15 18:25:04 +0200

    Query for searching drug names

--- src/models/drug.js
+++ src/models/drug.js
@@ -1,4 +1,5 @@
 var mongoose     = require('mongoose');
+var _ = require('underscore');
 var Schema       = mongoose.Schema;
 
 var DrugSchema   = new Schema({
@@ -58,7 +59,34 @@ DrugGroupSchema.statics.findByName = function(name, cb) {
 };
 
 DrugGroupSchema.statics.findDrugByName = function(name, cb){
-	return this.find({ 'drugAndTypes.drug.name': name}, 'drugAndTypes.drug.name', cb);
+    var pattern = new RegExp(name, 'i');
+	var matchingGroups = this.find({ 'drugAndTypes.drug.name': pattern}, 'drugAndTypes.drug.name', function(err, docs){
+        if(err){
+            cb(err, docs);
+        }
+
+        console.log('%s matching groups', docs.length)
+        /*for(var i = 0; i < docs.length; i++){
+            console.log(JSON.stringify(docs[i]))
+        }*/
+        /*var obj = docs[0].drugAndTypes[0].drug;
+        console.log(obj.toJSON().name);
+        var keys = Object.keys(obj);
+        console.log(JSON.stringify(keys))*/
+        var matchingNames = _.chain(docs)
+            //.filter(function(grp){ return grp.drugAndTypes; })
+            .map(function(grp){
+                return grp.drugAndTypes;
+            })
+            .flatten()
+            .filter(function(o){ return o && o.drug; })
+            .map(function(o){ return o.drug.toJSON().name; })
+            .filter(function(drugName){ return pattern.test(drugName)})
+            .value();
+        //console.log('matching names: %s', JSON.stringify(matchingNames))
+        //return matchingNames;*/
+        return cb(err, matchingNames);
+    });
 }
 
 module.exports = mongoose.model('DrugGroup', DrugGroupSchema, 'genericgroups');
